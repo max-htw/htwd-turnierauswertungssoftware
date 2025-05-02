53735d6af3137bdb4cb6490ce1fd50f7010c12d7
Maximilian Lohr <s86327@informatik.htw-dresden.de>
2025-05-02 11:16:42 +0200

    Turnierplan_Renderer_Max

--- src/production_se2/RoleTeam_TaskTurnierplan_Renderer_Max.java
+++ src/production_se2/RoleTeam_TaskTurnierplan_Renderer_Max.java
@@ -2,7 +2,19 @@ public class RoleTeam_TaskTurnierplan_Renderer_Max extends RoleTeam_TaskTurnierp
 
   @Override
   public StringBuilder renderResponse() {
-    return super.renderResponse();
-  }
+    StringBuilder r = new StringBuilder();
+
+    System.out.println(">>> renderResponse wurde aufgerufen!");
+
+    r.append("<h1>Turnierplan Übersicht</h1>\n");
+    r.append("<p>Team: ").append(daten.teamName).append("</p>\n");
+    r.append("<p>Gruppe: ").append(daten.gruppenName).append("</p>\n");
+
+    // hier die HTML-Tabelle aus den Daten anhängen
+    r.append(daten.htmlOfDerrivedClass());
+
+    return r;
+}
+
 
 }
